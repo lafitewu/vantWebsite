6b9843c253967934bc99de4043c3774e495bb7d0
lafitewu <863512936@qq.com>
2019-07-30 18:23:59 +0800

    首页静态页面完成

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -3,7 +3,7 @@
     .top
       .top_main
         .top_left koobinliu
-        .top_right
+        .top_right(@click="showPopup")
           i(class="iconfont iconicon_nav_icon_more")
     .middle
       .middle_username
@@ -52,13 +52,46 @@
         .look_more 
           | 查看更多
           i(class="iconfont iconicon_common_icon_home_turn_in")
+      .middle_file
+        .look_file 
+          .look_file_list
+            img(src="@/assets/images/icon_common_icon_folder_yellow.png")
+            span 关于弹窗规范的总结
+          .look_file_list
+            img(src="@/assets/images/icon_common_icon_folder_green.png")
+            span 关于弹窗规范的总结
+          .look_file_list
+            img(src="@/assets/images/icon_common_icon_folder_blue.png")
+            span 关于弹窗规范的总结
+          .look_file_more 
+            | 查看更多
+            i(class="iconfont iconicon_common_icon_home_turn_in")
+      .middle_icon
+        .middle_icon_list
+          i(class="iconfont iconicon_common_icon_home_dribbble icon1")
+          i(class="iconfont iconicon_common_icon_home_behance icon2")
+          i(class="iconfont iconicon_common_icon_home_zcool icon3")
+      .footer_font {{footerFont}}
+    //- 弹窗
+    van-popup(
+      v-model="popupShow"
+      position="right"
+      :style="{ width: '55%',height: '100%' }"
+    )
 </template>
 <script>
 export default {
   name: 'home',
   data () {
     return {
-      info: "做自己的设计，让别人无设计可做"
+      info: "做自己的设计，让别人无设计可做",
+      footerFont: "谢语：爱就像蓝天白云",
+      popupShow: false
+    }
+  },
+  methods: {
+    showPopup() {
+      this.popupShow = true
     }
   }
 }
@@ -68,7 +101,14 @@ export default {
       position: relative;
       width: 100%;
       background: #F3F4F6;
-      padding-bottom: 10vw;
+      padding-bottom: 6vw;
+      // 遮罩层样式
+      .van-overlay {
+        background: transparent;
+      }
+      .van-popup {
+        background: rgba(0,0,0,.5);
+      }
       // 顶部菜单
       .top {
         width: 100%;
@@ -194,10 +234,10 @@ export default {
         .middle_look {
           width: 90%;
           height: 98vw;
-          // background: red;
           margin: auto;
           margin-top: 6vw;
           border-top: 1px solid #eee;
+          border-bottom: 1px solid #eee;
           .look_frist {
             width: 100%;
             height: 24vw;
@@ -221,7 +261,6 @@ export default {
             width: 100%;
             height: 50vw;
             margin-top: 3.5vw;
-            background: #676B78;
             img {
               width: 100%;
               height: 100%;
@@ -238,6 +277,75 @@ export default {
             letter-spacing: 1.11px;
             cursor: pointer;
           }
+        }
+        .middle_file {
+          width: 90%;
+          margin: auto;
+          // 文件列表
+          .look_file {
+            width: 100%;
+            height: 55vw;
+            border-bottom: 1px solid #eee;
+            .look_file_list {
+              font-size: 3.7vw;
+              color: #242837;
+              letter-spacing: 1.11px;
+              line-height: 14vw;
+              cursor: pointer;
+              img {
+                position: relative;
+                width: 5.5vw;
+                top: 1vw;
+              }
+              span {
+                margin-left: 4vw;
+              }
+            }
+            &:nth-of-type(1) {
+              margin-top: 2vw;
+            }
+            .look_file_more {
+              float: right;
+              width: 30%;
+              font-size: 3.7vw;
+              color: #242837;
+              text-align: right;
+              margin-top: 1vw;
+              letter-spacing: 1.11px;
+              cursor: pointer;
+            }
+          }
+        }
+        // 图标列表
+        .middle_icon {
+          width: 90%;
+          margin: auto;
+          .middle_icon_list {
+            height: 15vw;
+            width: 100%;
+            line-height: 15vw;
+            i {
+              font-size: 6.5vw;
+              margin-right: 10vw;
+            }
+            .icon1 {
+              color: #d4237a;
+            }
+            .icon2 {
+              color: #1296db;
+            }
+            .icon3 {
+              color: #F5A623;
+            }
+          } 
+        }
+        // 谢语
+        .footer_font {
+          margin-top: 15vw;
+          text-align: center;
+          color: #676B78;
+          letter-spacing: 1.11px;
+          font-size: 3.7vw;
         }
       }
     }
